091d79d91df145d577622c9f23d055959dc19db4
Akshat Dubey <akshatdubey@outlook.com>
2015-09-30 22:14:00 +0800

    Initial Commit

---
+++ src/example/WebApp.java
@@ -0,0 +1,15 @@
+package example;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class WebApp extends HttpServlet{
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.getWriter().print("Fuck you");
+	}
+}
