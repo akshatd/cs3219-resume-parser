4ca9199c5d2eeccb19009eb62f796c9053a36765
rupali <rupali317@gmail.com>
2015-10-10 00:40:20 +0800

    finished implementing compareCVWithJob

--- src/Analyser/Analyser.java
+++ src/Analyser/Analyser.java
@@ -2,16 +2,21 @@ package Analyser;
 import java.sql.*;   // Use classes in java.sql package
 import java.util.ArrayList;
 
-import storage.CV;
-import storage.Job;
+import common.CV;
+import common.Job;
 
 
 public class Analyser 
 {
-	int matchedWords; 
+	int matchedWords = 0; 
 	int totalWords;
 	double percentage;
-	String [] JobKeyWordsArray;
+	String jobKeyWords;
+	String cvKeyWords;
+	String [] jobKeyWordsArray;
+	String [] cvKeyWordsArray;
+	Job job;
+	CV cv;
 	
 	private static Statement sqlStatement() {
 		try {
@@ -26,6 +31,15 @@ public class Analyser
 		}
 	}
 	
+	public int totalWordsInJob(int jobId, String [] jobKeyWordsArray)
+	{
+		int count = 0;
+		for(int i = 0; i < jobKeyWordsArray.length; i++)
+		{
+			count++;
+		}
+		return count;
+	}
 	
     //compare all CVs with 1 job description
 	public void compareCVWithJob(int jobId) throws SQLException
@@ -35,12 +49,27 @@ public class Analyser
 		String strSelect2 = "SELECT FROM COUNT(*) cv";
 		ResultSet rset1 = stmt.executeQuery(strSelect);
 		ResultSet rset2 = stmt.executeQuery(strSelect2);
-		JobKeyWordsArray = Job.getSkills(jobId);  //JobKeyWordsArray will be conceptualised | spearhead | .... | ....
-		for(int i = 0; i < JobKeyWordsArray.length; i++)
+		jobKeyWords = job.getSkills();  //JobKeyWordsArray will be conceptualised | spearhead | .... | ....
+		jobKeyWordsArray = jobKeyWords.split(",");
+		totalWords = totalWordsInJob(jobId, jobKeyWordsArray);
+		for(int i = 0; i < jobKeyWordsArray.length; i++)
 		{
-			for(int j = 0; j < rset1.getInt(); j++)
+			for(int j = 0; j < NumOfCol; j++)
 			{
-				
+				cvKeyWords = cv.getSkills();   // get
+				cvKeyWordsArray = cvKeyWords.split(",");
+				for(int k = 0; k < cvKeyWordsArray.length; k++)
+				{
+					if(jobKeyWordsArray[i] == cvKeyWordsArray[k])
+					{
+						matchedWords++;
+					}
+					else
+					{
+						continue;
+					}
+				}
+				percentage = calculatePercentage(matchedWords, totalWords);
 			}
 		}
 		
