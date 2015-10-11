f7fcf27b57fccd4d7d79a64787770a735555b9e3
Akshat Dubey <akshatdubey@outlook.com>
2015-10-11 11:27:06 +0800

    completed servlet mapping

--- src/main/ResumeParser.java
+++ src/main/ResumeParser.java
@@ -8,7 +8,12 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class ResumeParser extends HttpServlet{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7269929250136699541L;
+
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		resp.getWriter().print("Fuck you");
+		resp.getWriter().print("hai");
 	}
 }
