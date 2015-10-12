b55664632fcf5e70c77e4e3987a2f0b049051e4d
Akshat Dubey <akshatdubey@outlook.com>
2015-10-12 18:18:10 +0800

    Finally resolved all conflicts

--- src/common/CV.java
+++ src/common/CV.java
@@ -5,52 +5,26 @@ import java.util.List;
 import java.util.Map;
 
 public class CV {
-<<<<<<< HEAD
 	private int id;
 	private String firstName;
 	private String lastName;
-	private int age;
 	
 	private Map<String, List<Word>> cvContentMap;
-=======
-	int id;
-	String name;
-	String education;
-	String skills;
-	String experience;
-	String leadership;
-	int age;
->>>>>>> master
 	
 	public boolean setId(int i) {
 		id = i;
 		cvContentMap = new HashMap<String, List<Word>>();
 		return true;
 	}
-	public boolean setName(String nm) {
-		name = nm;
+	public boolean setFirstName(String nm) {
+		firstName = nm;
 		return true;
 	}
-	public boolean setExperience(String exp) {
-		experience = exp;
+	public boolean setLastName(String nm) {
+		lastName = nm;
 		return true;
 	}
-	public boolean setEducation(String edu) {
-		education = edu;
-		return true;
-	}
-	public boolean setSkills(String sk) {
-		skills = sk;
-		return true;
-	}
-	public boolean setLeadership(String lead) {
-		leadership = lead;
-		return true;
-	}
-	public boolean setAge(int a) {
-		age = a;
-		return true;
-	}
+	
 	public void setCvContentMap(Map<String, List<Word>> cvContentMap) {
 		this.cvContentMap = cvContentMap;
 	}
@@ -58,23 +32,12 @@ public class CV {
 	public int getId() {
 		return id;
 	}
-	public String getName() {
-		return name;
+	
+	public String getFirstName() {
+		return firstName;
 	}
-	public String getSkills() {
-		return skills;
-	}
-	public String getExperience() {
-		return experience;
-	}
-	public String getEducation() {
-		return education;
-	}
-	public String getLeadership() {
-		return leadership;
-	}
-	public int getAge() {
-		return age;
+	public String getLastName() {
+		return lastName;
 	}
 	public Map<String, List<Word>> getCvContentMap() {
 		return cvContentMap;
--- src/common/Job.java
+++ src/common/Job.java
@@ -6,14 +6,7 @@ import java.util.Map;
 
 public class Job {
 	
-    int id;
-	String title;
-	String skills;
-	String education;
-	String location;
-	String experience;
-	String leadership;
-	
+    int id;	
 	private Map<String, List<Word>> contentMap;
 	
 	public Job(){
@@ -23,31 +16,7 @@ public class Job {
 	public boolean setId(int i) {
 		id = i;
 		return true;
-	}
-	public boolean setTitle(String t) {
-		title = t;
-		return true;
-	}
-	public boolean setSkills(String s) {
-		skills = s;
-		return true;
-	}
-	public boolean setEducation(String ed) {
-		education = ed;
-		return true;
-	}
-	public boolean setLocation(String l) {
-		location = l;
-		return true;
-	}
-	public boolean setExperience(String ex) {
-		experience = ex;
-		return true;
-	}
-	public boolean setLeadership(String lead) {
-		leadership = lead;
-		return true;
-	}
+	}	
 	
 	public void setContentMap(Map<String, List<Word>> contentMap) {
 		this.contentMap = contentMap;
@@ -56,27 +25,8 @@ public class Job {
 	public int getId() {
 		return id;
 	}
-	public String getTitle() {
-		return title;
-	}
-	public String getSkills() {
-		return skills;
-	}
-	public String getEducation() {
-		return education;
-	}
-	public String getLocation() {
-		return location;
-	}
-	public String getExperience() {
-		return experience;
-	}
-<<<<<<< HEAD
+	
 	public Map<String, List<Word>> getContentMap() {
 		return this.contentMap;
-=======
-	public String getLeadership() {
-		return leadership;
->>>>>>> master
 	}
 }
