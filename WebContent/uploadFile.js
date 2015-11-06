73e8b16366dc3094190f172c60885c6ae2a7de13
rupali <rupali317@gmail.com>
2015-11-06 23:40:33 +0800

    angular js to add rows dynamically

--- WebContent/uploadFile.js
+++ WebContent/uploadFile.js
@@ -1,16 +1,27 @@
 var myfile="";
+var resume = angular.module("resume", []);
+resume.controller("control", function($scope)
+{
+$scope.records = [{ 'num':'1','name':'Rupali.pdf'},];
+
+$scope.addRow = function()
+{
+	$scope.records.push({ 'num':$scope.num,'name':$scope.name });
+	//$scope.name='';
+	//$scope.num = 0;
+};
 
 $('#resume_link').click(function( e ) {
     e.preventDefault();
     $('#resume').trigger('click');
-});
 
+});
 $('#resume').on( 'change', function() {
    myfile= $( this ).val();
    console.log("myfile"+myfile);
        //upload it
        var x = document.getElementById("resume");
-       //var fileNo = "";
+       var fileNo = "";
        var fileName = "";
        var fileSize = "";
        if ('files' in x)
@@ -18,21 +29,59 @@ $('#resume').on( 'change', function() {
 
             for (var i = 0; i < x.files.length; i++)
             {
-                //fileNo += "<br><strong>" + (i+1) + "</strong><br>";
+                fileNo += "<strong>" + (i+1) + "</strong>";
                 var file = x.files[i];
                 if ('name' in file)
                 {
-                    fileName = "<a download='"+file.name+"'>"+file.name+"</a>" ;
-                }
-                if ('size' in file)
-                {
-                    fileSize = file.size;
+                    //fileName = "<a href='"+file.name+"'>"+file.name+"</a>" ;
+                    fileName = file.name;
                 }
             }
         }
-        //document.getElementById("num").innerHTML = fileNo;
-        document.getElementById("name").innerHTML = fileName;
-        document.getElementById("size").innerHTML = fileSize;
+        
+        $scope.records.push({ 'num':fileNo,'name':fileName });
 
    }
 );
+
+});
+
+
+// $('#resume_link').click(function( e ) {
+//     e.preventDefault();
+//     $('#resume').trigger('click');
+//
+// });
+
+// $('#resume').on( 'change', function() {
+//    myfile= $( this ).val();
+//    console.log("myfile"+myfile);
+//        //upload it
+//        var x = document.getElementById("resume");
+//        var fileNo = "";
+//        var fileName = "";
+//        var fileSize = "";
+//        if ('files' in x)
+//        {
+//
+//             for (var i = 0; i < x.files.length; i++)
+//             {
+//                 fileNo += "<strong>" + (i+1) + "</strong>";
+//                 var file = x.files[i];
+//                 if ('name' in file)
+//                 {
+//                     //fileName = "<a href='"+file.name+"'>"+file.name+"</a>" ;
+//                     fileName = file.name;
+//                 }
+//                 if ('size' in file)
+//                 {
+//                     fileSize = file.size;
+//                 }
+//             }
+//         }
+//         document.getElementById("num").innerHTML = fileNo;
+//         document.getElementById("name").innerHTML = fileName;
+//         document.getElementById("size").innerHTML = fileSize;
+//
+//    }
+// );
