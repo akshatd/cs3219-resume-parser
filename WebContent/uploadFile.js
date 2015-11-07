bda6a5f9cd415c43f5f8240ecf5feba2244a0d19
rupali <rupali317@gmail.com>
2015-11-07 11:49:16 +0800

    segregated recruiter & applicant in terms of js

--- WebContent/uploadFile.js
+++ WebContent/uploadFile.js
@@ -1,15 +1,20 @@
 var myfile="";
 var counter = 0;
-$('#resume_link').click(function( e ) {
+$('#resume_link1').click(function( e ) {
     e.preventDefault();
-    $('#resume').trigger('click');
+    $('#resume_applicant').trigger('click');
 
 });
-$('#resume').on( 'change', function() {
+$('#resume_link2').click(function( e ) {
+    e.preventDefault();
+    $('#resume_recruiter').trigger('click');
+
+});
+$('#resume_applicant').on( 'change', function() {
    myfile= $( this ).val();
    console.log("myfile"+myfile);
        //upload it
-       var x = document.getElementById("resume");
+       var x = document.getElementById("resume_applicant");
        var fileNo = "";
        var fileName = "";
        var fileSize = "";
@@ -27,6 +32,35 @@ $('#resume').on( 'change', function() {
                 }
             }
             var newRow = jQuery('<tr><td>'+fileNo+'</td><td>'+fileName+'</td></tr>');
+            jQuery('tbody.list').append(newRow);
+
+        }
+
+   }
+);
+
+$('#resume_recruiter').on( 'change', function() {
+   myfile= $( this ).val();
+   console.log("myfile"+myfile);
+       //upload it
+       var x = document.getElementById("resume_recruiter");
+       var fileNo = "";
+       var fileName = "";
+       var fileSize = "";
+       counter++;
+       if ('files' in x)
+       {
+
+            for (var i = 0; i < x.files.length; i++)
+            {
+                fileNo += counter;
+                var file = x.files[i];
+                if ('name' in file)
+                {
+                    fileName = file.name;
+                }
+            }
+            var newRow = jQuery('<tr><td>'+fileNo+'</td><td><a>'+fileName+'</a></td></tr>');
             jQuery('tbody.list').append(newRow);
 
         }
